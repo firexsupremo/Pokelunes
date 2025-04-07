4356cefaf1cb3a980741368281a6d073d5f9c7f4
firexsupremo <130928759+firexsupremo@users.noreply.github.com>
2025-04-07 20:12:18 +0000

    main

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react' 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Lista from './componentes/Lista'
+import Capturados from './componentes/Capturados'
+import Favoritos from './componentes/Favoritos'
+import Aleatorios from './componentes/Aleatorios'
+import Usuarios from './componentes/Usuarios'
+import Pokemon from './componentes/Pokemon'
+import Menu from './componentes/Menu'
+
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <Router>
+        <Menu />
+        
+        <Routes>
+        <Route path="/" element={<Lista />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/aleatorios" element={<Aleatorios />} />
+        <Route path="/capturados" element={<Capturados />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+
+       </Routes>
+
+       
+  </Router>
+);
+}
+
+export default App;
---
+++ src/componentes/Favoritos/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react' 
+import './style.css'
+
+function Favoritos() {
+  
+  return (
+     <>
+     <h1>Favoritos</h1>
+     </>)
+   }
+  
+export default Favoritos;
